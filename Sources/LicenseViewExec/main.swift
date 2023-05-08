df25ff2a026528637cfaf105d0bcb92f4272e768
zunda <zunda.dev@gmail.com>
2023-05-08 15:39:11 +0900

    remove Model

--- Sources/LicenseViewExec/main.swift
+++ Sources/LicenseViewExec/main.swift
@@ -16,12 +16,12 @@ func generateSourceCode(packages: [WorkSpacePackage: String]) -> String {
 import Foundation
   
 enum LicenseList: CaseIterable {
-  static let packages: [PackageModel] = [
+  static let packages: [Package] = [
     \(workspaceInits.joined(separator: ",\n"))
   ]
 }
 
-struct PackageModel: Hashable, Identifiable {
+struct Package: Hashable, Identifiable {
   let id = UUID()
   let name: String
   let location: URL
